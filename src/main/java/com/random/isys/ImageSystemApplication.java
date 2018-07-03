deb1c52fbf9ad91a2b82dd48c94b744c933dbd95
92649264634 <40713500+92649264634@users.noreply.github.com>
2018-07-03 12:40:11 +0800

    项目初始化

---
+++ src/main/java/com/random/isys/ImageSystemApplication.java
@@ -0,0 +1,12 @@
+package com.random.isys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageSystemApplication.class, args);
+	}
+}
